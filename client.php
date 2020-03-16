1f2cc34b41db2430852426e8e445996a1f2e1c23
Kirill <kirill.lihotin@gmail.com>
2020-03-16 16:44:24 +0500

    5

--- client.php
+++ client.php
@@ -2,7 +2,9 @@
 
 $host    = "127.0.0.1";
 $port    = 35000;
+
 $message = "Hello Server";
+
 echo "Message To server :".$message;
 
 $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
