95ef522cf9374570107612de4f9673d7f99e53c2
Kirill <kirill.lihotin@gmail.com>
2020-03-17 16:06:33 +0500

    11

--- client.php
+++ client.php
@@ -2,11 +2,14 @@
 
 require_once "./config.php";
 
+$address = '192.168.1.53';
+$port = 10000;
+
 $message = "Hello Server";
 echo "Message To server :".$message;
 
 $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
 
-$result = socket_connect($socket, $host, $port) or die("Could not connect to server\n");  
+$result = socket_connect($socket, $address, $port) or die("Could not connect to server\n");  
 
 socket_close($socket);
