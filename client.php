0c2fc900148eb5780f6aeda624e506cdcedd3f01
Kirill <kirill.lihotin@gmail.com>
2020-03-17 16:51:50 +0500

    14

--- client.php
+++ client.php
@@ -4,11 +4,11 @@ require_once "./config.php";
 
 $message = "Hello Server";
 echo "Message To server :".$message;
-// create socket
+
 $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
-// connect to server
+
 $result = socket_connect($socket, $address, $port) or die("Could not connect to server\n");  
-// send string to server
+
 socket_write($socket, $message, strlen($message)) or die("Could not send data to server\n");
 
 socket_close($socket);
--- server.php
+++ server.php
@@ -1,20 +1,20 @@
 <?php
 
 require_once "./config.php";
-// don't timeout!
+
 set_time_limit(0);
-// create socket
+
 $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
-// bind socket to port
+
 $result = socket_bind($socket, $address, $port) or die("Could not bind to socket\n");
-// start listening for connections
+
 $result = socket_listen($socket, 3) or die("Could not set up socket listener\n");
 
 while (true) {
     $spawn = socket_accept($socket) or die("Could not accept incoming connection\n");
-    // read client input
+
     $input = socket_read($spawn, 1024) or die("Could not read input\n");
-    // clean up input string
+
     $input = trim($input);
     echo "Client Message : ".$input;
 }
