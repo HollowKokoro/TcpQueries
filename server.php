4ab6e014212d3d40760700ca9197a16992e59947
HollowKokoro <kirill.lihotin@gmail.com>
2020-03-18 21:20:14 +0500

    16

--- server.php
+++ server.php
@@ -6,15 +6,28 @@ set_time_limit(0);
 
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket\n");
 
-$result = socket_bind($socket, $address, $port) or die("Could not bind to socket\n");
+socket_bind($socket, $address, $port) or die("Could not bind to socket\n");
 
-$result = socket_listen($socket, 3) or die("Could not set up socket listener\n");
+socket_listen($socket, 3) or die("Could not set up socket listener\n");
 
+socket_set_nonblock($socket);
+
+// Clients
+$clients = [];
+
+// Loop continuously
 while (true) {
-    $spawn = socket_accept($socket) or die("Could not accept incoming connection\n");
-
-    $input = socket_read($spawn, 1024) or die("Could not read input\n");
-
-    $input = trim($input);
-    echo "Client Message : ".$input;
+    // Accept new connections
+    if ($newSocket = socket_accept($socket)) {
+        if (is_resource($newSocket)) {
+            // Write something back to the user
+            socket_write($newSocket, ">", 1).chr(0);
+            // Non bloco for the new connection
+            socket_set_nonblock($newSocket);
+            // Do something on the server side
+            echo "New client connected\n";
+            // Append the new connection to the clients array
+            $clients[] = $newSocket;
+        }
+    }
 }
