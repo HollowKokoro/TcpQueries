bb7984e0fdf0ae2ab4c0e2f1878b0829ca34bec1
Kirill <kirill.lihotin@gmail.com>
2020-03-24 14:55:21 +0500

    input

--- client.php
+++ client.php
@@ -2,14 +2,13 @@
 
 require_once "./config.php";
 
-$message = "Hello Server";
-echo "Message To server :".$message;
+$input = readline ();
 
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket\n");
 
 $result = socket_connect($socket, $address, $port) or die("Could not connect to server\n");  
 
 while (true) {
-    socket_write($socket, $message, strlen($message)) or die("Could not send data to server\n");
+    socket_write($socket, $input, strlen($input)) or die("Could not send data to server\n");
     sleep(1);
 }
