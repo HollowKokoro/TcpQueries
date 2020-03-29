160b5502ff3b5ef9ceb9c986fcd2e6a24dcaa994
HollowKokoro <kirill.lihotin@gmail.com>
2020-03-29 18:27:17 +0500

    26

--- client.php
+++ client.php
@@ -2,13 +2,12 @@
 
 require_once "./config.php";
 
-$input = readline();
-
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket\n");
-
-$result = socket_connect($socket, $address, $port) or die("Could not connect to server\n");
+socket_connect($socket, $address, $port) or die("Could not connect to server\n");
 
 while (true) {
+    $input = readline();
+    $input .="\n";
     socket_write($socket, $input, strlen($input)) or die("Could not send data to server\n");
     sleep(1);
 }
--- server.php
+++ server.php
@@ -5,32 +5,20 @@ require_once "./config.php";
 set_time_limit(0);
 
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket\n");
-
 socket_bind($socket, $address, $port) or die("Could not bind to socket\n");
-
 socket_listen($socket, 3) or die("Could not set up socket listener\n");
-
-socket_set_nonblock($socket);
 
 $clients = [];
 
 while (true) {
-    if ($newSocket = socket_accept($socket)) {
-        if (is_resource($newSocket)) {
-            socket_set_nonblock($newSocket);
-            echo "New client connected\n";
-            $clients[] = $newSocket;
+    $newSocket = socket_accept($socket) or die("Could not accept incoming connection\n");
+    if (is_resource($newSocket)) {
+        //socket_set_nonblock($newSocket);
+        echo "New client connected\n";
+        $clients[] = $newSocket;
+        while(($read = socket_read($newSocket, 1024, PHP_NORMAL_READ)) !== false) {
+            echo "Read: $read\n";
         }
     }
-    if (count($clients)) {
-        foreach ($clients as $k => $v) {
-            $string = '';
-            if ($char = socket_read($v, 1024)) {
-                $string .= $char;
-            }
-            echo "$k:$string\n";
-        }
-    }
-    sleep(1);
 }
-socket_close($sock);
+socket_close($socket);
