b4c087954e5c300e542510ab99f42a53fe41f156
HollowKokoro <kirill.lihotin@gmail.com>
2020-03-30 13:29:00 +0500

    multiple connections + input

--- client.php
+++ client.php
@@ -2,12 +2,11 @@
 
 require_once "./config.php";
 
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket\n");
-socket_connect($socket, $address, $port) or die("Could not connect to server\n");
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket\n" . socket_strerror(socket_last_error()) . "\n");
+socket_connect($socket, $address, $port) or die("Could not connect to server\n" . socket_strerror(socket_last_error($socket)) . "\n");
 
 while (true) {
     $input = readline();
     $input .="\n";
-    socket_write($socket, $input, strlen($input)) or die("Could not send data to server\n");
-    sleep(1);
+    socket_write($socket, $input, strlen($input)) or die("Could not send data to server\n" . socket_strerror(socket_last_error($socket)) . "\n");
 }
--- server.php
+++ server.php
@@ -2,21 +2,54 @@
 
 require_once "./config.php";
 
+ini_set('error_reporting', E_ALL ^ E_NOTICE);
+ini_set('display_errors', 1);
+
 set_time_limit(0);
 
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket\n");
-socket_bind($socket, $address, $port) or die("Could not bind to socket\n");
-socket_listen($socket, 3) or die("Could not set up socket listener\n");
+ob_implicit_flush();
 
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket\n" . socket_strerror(socket_last_error()) . "\n");
+socket_bind($socket, $address, $port) or die("Could not bind to socket\n" . socket_strerror(socket_last_error($socket)) . "\n");
+socket_listen($socket, 3) or die("Could not set up socket listener\n" . socket_strerror(socket_last_error($socket)) . "\n");
+socket_set_nonblock($socket);
 $clients = [];
 
 while (true) {
-    $newSocket = socket_accept($socket) or die("Could not accept incoming connection\n");
-    if (is_resource($newSocket)) {
-        echo "New client connected\n";
-        $clients[] = $newSocket;
-        while(($read = socket_read($newSocket, 1024, PHP_NORMAL_READ)) !== false) {
-            echo "Read: $read\n";
+    if ($newSocket = socket_accept($socket)) {
+        if (is_resource($newSocket)) {
+            socket_write($newSocket, ">", 1).chr(0);
+            socket_set_nonblock($newSocket);
+            echo "New client connected\n";
+            $clients[] = $newSocket;
         }
     }
+
+    if (count($clients)) {
+        foreach ($clients as $k => $v) {
+            $string = '';
+            if ($char = socket_read($v, 1024)) {
+                $string .= $char;
+            }
+
+            if ($string) {
+                echo "$k:$string\n";
+            } else {
+                if ($seconds > 60) {
+                    if (false === socket_write($v, 'PING')) {
+                        socket_close($clients[$k]);
+
+                        unset($clients[$k]);
+                    }
+
+                    $seconds = 0;
+                }
+            }
+        }
+    }
+
+    sleep(1);
+
+    $seconds++;
 }
+socket_close($sock);
