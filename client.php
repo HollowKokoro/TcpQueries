fe5ed3e2a63861549be34d3a428b3ceea0b844c4
Kokoro <kirill.lihotin@gmail.com>
2020-04-28 22:58:29 +0500

    38

--- client.php
+++ client.php
@@ -1,13 +1,13 @@
 <?php
+//declare(strict_types = 1);
 
 $config = require_once "./config.php";
-require_once "./nonBlockingReadLine.php";
 
 class Client
 {
     private $socket;
 
-    public function __construct($address, $port)
+    public function __construct(string $address, int $port)
     {
         $this->socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket\n" . socket_strerror(socket_last_error()) . "\n");
         socket_connect($this->socket, $address, $port) or die("Could not connect to server\n" . socket_strerror(socket_last_error($this->socket)) . "\n");
@@ -17,21 +17,19 @@ class Client
     {
         while(true) {
             $input = readline();
-            if ($this->nonBlockRead(STDIN, $input)) {
+            if ($this->nonBlockRead()) {
                 echo "Input: " . $input . "\n";
             }
-            if (socket_select($input, $write = NULL, $except = NULL, 0) < 1) {
-                $this->readInput();
-            }     
+            if (socket_select([$this->socket], $write = NULL, $except = NULL, 0) > 0) {
+                $this->readFromServer();
+            }
+        usleep(500);
         }
     }
 
-    private function nonBlockRead($fd, &$data)
+    private function nonBlockRead()
     {
-        $read = array($fd);
-        $write = array();
-        $except = array();
-        $result = stream_select($read, $write, $except, 0);
+        $result = stream_select($read = [STDIN], $write = null, $except = null, 0);
         
         if ($result === false) { 
             throw new RunTimeException("Can not select stream STDIN");
@@ -41,18 +39,23 @@ class Client
             return false;
         }
         
-        $data = stream_get_line($fd, 1);
-        return $data;
+        $stdinBuffer = "";
+        while($stdinData = stream_get_line(STDIN, 1024, "\r\n")) {
+            $stdinBuffer .= $stdinData;
+        }
+        return $stdinData;
     }
     
-    private function readInput()
+    private function readFromServer(): string
     {
+        $buffer = "";
         while(($read = socket_read($this->socket, 1024, PHP_NORMAL_READ)) !== false) {
-            $str .= $read;
-            if (strpos($str, "\n") !== false) {
+            if ($read == "") {
                 break;
             }
+            $buffer .= $read;
         }
+        return $buffer;
     }
 }
 $connect = new Client($config["address"], $config["port"]);
